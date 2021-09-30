0fc69b1676989c8672894f508c364a9635683336
Anand Pap <anandpap@live.com>
2021-09-30 06:57:21 +0200

    Destructuring assignment

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -7,8 +7,9 @@ const MY_CAR = {
 function main(myCar) {
     // Only change code below this line
 
-    const {wheels, cylinders, size} = myCar
-    return [cylinders, size];
+    const {wheels, cylinders, size} = myCar;
+    
+    return {cylinders, size};
     // Only change code above this line
 }
 
